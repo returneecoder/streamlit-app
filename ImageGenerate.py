f91a43951d37abc1be458a53af6b001bd4aaa0fe
Amna Akhtar <106440495+returneecoder@users.noreply.github.com>
2025-07-15 15:54:03 +0400

    Update ImageGenerate.py

--- ImageGenerate.py
+++ ImageGenerate.py
@@ -1,8 +1,9 @@
 import streamlit as st
-from google import  genai
+#from google import  genai
 from PIL import Image
-from google.genai import types
+#from google.genai import types
 from io import BytesIO
+import google.generativeai as genai
 
 
 def show_image():
